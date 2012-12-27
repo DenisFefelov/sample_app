3e50c55b9920d7974940e04977072e76016a1bb5
DenisFefelov <fefelov_denis@mail.ru>
2012-12-27 21:07:49 +0400

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,9 +10,9 @@ module SessionsHelper
   end
   
   def current_user
-    @current_user ||= user_from_remember_token
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
-  
+
   def current_user?(user)
     user == current_user
   end
